8be04d6346773a47cfa20b5fc530767c2e0c6730
Jayapriya013 <146405871+Jayapriya013@users.noreply.github.com>
2025-05-13 20:30:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("✈️ Flight Arrival Delay Predictor (Dataset-Based)")
 # Load dataset
 @st.cache_data
 def load_data():
-    return pd.read_csv("https://github.com/Jayapriya013/fd/blob/main/final_airline_times_HHMM%20(2).csv")
+    return pd.read_csv("https://github.com/Jayapriya013/fd/blob/main/final_airline_times_HHMM(2).csv")
 df = load_data()
 
 # User Inputs
